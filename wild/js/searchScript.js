86a8464420cd0f95e8bf96f5421e98d6798907fc
RIQUELME EDUARDO PREVITALE GOMES <1050292311030@fatec>
2025-04-03 11:36:55 -0300

    wild

--- wild/js/searchScript.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("search").addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) {
-      event.preventDefault();
-      
-      console.log("Usuário pressionou Enter. Execute a busca aqui.");
-      
-  }
-});
